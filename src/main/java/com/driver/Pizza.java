397c9db096483ca3a73af870fefd88701549f8d0
DivyaReddy1998 <102896700+DivyaReddy1998@users.noreply.github.com>
2023-02-16 20:21:44 +0530

    Update Pizza.java
    change

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -73,10 +73,10 @@ public class Pizza {
             if(isTakeAway){
                 this.bill += "Paperbag Added: 20"+"\n";
             }
-            this.bill += "Total Price: " +this.price;
+            this.bill += "Total Price: "+this.price+"\n";
             this.isBillCreated = true;
             return this.bill;
         }else
-        return "";
+        return this.bill;
     }
 }
